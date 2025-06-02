dd5402a1ed2f133a262b220b47ad23c968e5dfdf
PaigeSylvan <paigesylvan@gmail.com>
2025-06-02 13:21:00 -0500

    updated footer img

--- src/components/footer.js
+++ src/components/footer.js
@@ -22,7 +22,7 @@ const Footer = () => {
       </div>
     
       <div className="hidden lg:block  item-center">
-        <iframe className="lg:h-[280px] lg:w-[400px] " src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2915.9049459146577!2d-88.26684000843377!3d43.04343141724433!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8805af20e8430043%3A0xa6e0dac49ec95e18!2sQuick%20Cleaners%20%26%20Laundry!5e0!3m2!1sen!2sus!4v1714941653774!5m2!1sen!2sus" referrerpolicy="no-referrer-when-downgrade" title=" " ></iframe>
+        <iframe className="lg:h-[280px] lg:w-[300px] " src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2915.9049459146577!2d-88.26684000843377!3d43.04343141724433!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8805af20e8430043%3A0xa6e0dac49ec95e18!2sQuick%20Cleaners%20%26%20Laundry!5e0!3m2!1sen!2sus!4v1714941653774!5m2!1sen!2sus" referrerpolicy="no-referrer-when-downgrade" title=" " ></iframe>
       </div>
     </div>
   )
