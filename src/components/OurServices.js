a29372b6edbbf528d22aecf1b8b4f34695f17e03
PaigeSylvan <paigesylvan@gmail.com>
2025-11-12 13:45:22 -0600

    added google analytics to React app

--- src/components/OurServices.js
+++ src/components/OurServices.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 
 import services1 from '../styles/images/services-1.jpg';
 import services2 from '../styles/images/services-2.jpg';
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './App.css';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-RDRK7RLRW9');
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +16,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: measure performance
 reportWebVitals();
